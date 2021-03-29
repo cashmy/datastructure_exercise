08c265f1ff67edc360b2c20f8a62ab12760a8528
cashmy <cmyers880@gmail.com>
2021-03-29 13:49:16 -0500

    Rtv National Pi Day month: using a Tuple

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import my_family
+import retrieve_month
 
 
 def print_hi(name):
@@ -8,4 +9,5 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('Student')
 
+    retrieve_month.get_month()
     my_family.setup()
---
+++ retrieve_month.py
@@ -0,0 +1,13 @@
+
+months = ("January", "February", "March",
+          "April", "May", "June",
+          "July", "August", "September",
+          "October", "November", "December")
+
+pi = 3.14
+
+month_number = int(pi - 1)
+
+
+def get_month():
+    print(f'\n\nThe month of National Pi day is {months[month_number]}')
