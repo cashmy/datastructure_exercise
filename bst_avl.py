c6ca043afa2af30294c38932f6c6c6681bdcb247
cashmy <cmyers880@gmail.com>
2021-03-30 15:54:03 -0500

    Added determination and internal notation for rotation of branches.

--- bst_avl.py
+++ bst_avl.py
@@ -26,13 +26,30 @@ class BstTree:
 
         # Now determine the balance factor
         balance = self.get_balance(node)
+        print(f'Balance is {balance}')
+        # Now determine the rotation method: LEFT-LEFT, LEFT-RIGHT, RIGHT-RIGHT, RIGHT-LEFT
+        # LEFT-LEFT
+        if balance > 1 and data < node.left_child_node.data:
+            return self.right_rotate(node)
+        # RIGHT-RIGHT
+        if balance < -1 and data > node.right_child_node.data:
+            return self.left_rotate(node)
+        # LEFT-RIGHT
+        if balance > 1 and data > node.left_child_node.data:
+            node.left_child_node = self.left_rotate(node.left_child_node)
+            return self.right_rotate(node)
+        # RIGHT-LEFt
+        if balance < -1 and data < node.right_child_node.data:
+            node.right_child_node = self.right_rotate(node.right_child_node)
+            return self.left_rotate(node)
+
         return node
 
     def left_rotate(self, node):
-        pass
+        return node
 
     def right_rotate(self, node):
-        pass
+        return node
 
     @staticmethod
     def get_height(node):
@@ -51,3 +68,44 @@ class BstTree:
         print("{0} ".format(node.data), end="")
         self.print_tree_pre_order(node.left_child_node)
         self.print_tree_pre_order(node.right_child_node)
+
+# a) Left Left Case
+#
+# T1, T2, T3 and T4 are subtrees.
+#          z                                      y
+#         / \                                   /   \
+#        y   T4      Right Rotate (z)          x      z
+#       / \          - - - - - - - - ->      /  \    /  \
+#      x   T3                               T1  T2  T3  T4
+#     / \
+#   T1   T2
+#
+# b) Left Right Case
+#
+#      z                               z                           x
+#     / \                            /   \                        /  \
+#    y   T4  Left Rotate (y)        x    T4  Right Rotate(z)    y      z
+#   / \      - - - - - - - - ->    /  \      - - - - - - - ->  / \    / \
+# T1   x                          y    T3                    T1  T2 T3  T4
+#     / \                        / \
+#   T2   T3                    T1   T2
+#
+# c) Right Right Case
+#
+#   z                                y
+#  /  \                            /   \
+# T1   y     Left Rotate(z)       z      x
+#     /  \   - - - - - - - ->    / \    / \
+#    T2   x                     T1  T2 T3  T4
+#        / \
+#      T3  T4
+#
+# d) Right Left Case
+#
+#    z                            z                            x
+#   / \                          / \                          /  \
+# T1   y   Right Rotate (y)    T1   x      Left Rotate(z)   z      y
+#     / \  - - - - - - - - ->     /  \   - - - - - - - ->  / \    / \
+#    x   T4                      T2   y                  T1  T2  T3  T4
+#   / \                              /  \
+# T2   T3                           T3   T4
