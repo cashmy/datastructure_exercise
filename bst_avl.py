1af2f7aa5497b24cb603fc26f029d7d2d1f1ec8b
cashmy <cmyers880@gmail.com>
2021-03-30 21:05:08 -0500

    Added rotation functionality to balance BST

--- bst_avl.py
+++ bst_avl.py
@@ -26,7 +26,6 @@ class BstTree:
 
         # Now determine the balance factor
         balance = self.get_balance(node)
-        print(f'Balance is {balance}')
         # Now determine the rotation method: LEFT-LEFT, LEFT-RIGHT, RIGHT-RIGHT, RIGHT-LEFT
         # LEFT-LEFT
         if balance > 1 and data < node.left_child_node.data:
@@ -45,11 +44,38 @@ class BstTree:
 
         return node
 
-    def left_rotate(self, node):
-        return node
+    def left_rotate(self, node_z):
+        node_y = node_z.right_child_node
+        subtree_node_2 = node_y.left_child_node
 
-    def right_rotate(self, node):
-        return node
+        # Perform rotation
+        node_y.left_child_node = node_z
+        node_z.right_child_node = subtree_node_2
+
+        # Update heights
+        node_z.height = 1 + max(self.get_height(node_z.left_child_node),
+                                self.get_height(node_z.right_child_node))
+        node_y.height = 1 + max(self.get_height(node_y.left_child_node),
+                                self.get_height(node_y.right_child_node))
+
+        return node_y
+
+    def right_rotate(self, node_z):
+        node_y = node_z.left_child_node
+        subtree_node_3 = node_y.right_child_node
+
+        # Perform rotation
+        node_y.right_child_node = node_z
+        node_z.left_child_node = subtree_node_3
+
+        # Update heights
+        node_z.height = 1 + max(self.get_height(node_z.left_child_node),
+                                self.get_height(node_z.right_child_node))
+        node_y.height = 1 + max(self.get_height(node_y.left_child_node),
+                                self.get_height(node_y.right_child_node))
+
+        # Return the new root
+        return node_y
 
     @staticmethod
     def get_height(node):
@@ -69,43 +95,43 @@ class BstTree:
         self.print_tree_pre_order(node.left_child_node)
         self.print_tree_pre_order(node.right_child_node)
 
-# a) Left Left Case
-#
-# T1, T2, T3 and T4 are subtrees.
-#          z                                      y
-#         / \                                   /   \
-#        y   T4      Right Rotate (z)          x      z
-#       / \          - - - - - - - - ->      /  \    /  \
-#      x   T3                               T1  T2  T3  T4
-#     / \
-#   T1   T2
-#
-# b) Left Right Case
-#
-#      z                               z                           x
-#     / \                            /   \                        /  \
-#    y   T4  Left Rotate (y)        x    T4  Right Rotate(z)    y      z
-#   / \      - - - - - - - - ->    /  \      - - - - - - - ->  / \    / \
-# T1   x                          y    T3                    T1  T2 T3  T4
-#     / \                        / \
-#   T2   T3                    T1   T2
-#
-# c) Right Right Case
-#
-#   z                                y
-#  /  \                            /   \
-# T1   y     Left Rotate(z)       z      x
-#     /  \   - - - - - - - ->    / \    / \
-#    T2   x                     T1  T2 T3  T4
-#        / \
-#      T3  T4
-#
-# d) Right Left Case
-#
-#    z                            z                            x
-#   / \                          / \                          /  \
-# T1   y   Right Rotate (y)    T1   x      Left Rotate(z)   z      y
-#     / \  - - - - - - - - ->     /  \   - - - - - - - ->  / \    / \
-#    x   T4                      T2   y                  T1  T2  T3  T4
-#   / \                              /  \
-# T2   T3                           T3   T4
+    # a) Left Left Case
+    #
+    # T1, T2, T3 and T4 are subtrees.
+    #          z                                      y
+    #         / \                                   /   \
+    #        y   T4      Right Rotate (z)          x      z
+    #       / \          - - - - - - - - ->      /  \    /  \
+    #      x   T3                               T1  T2  T3  T4
+    #     / \
+    #   T1   T2
+    #
+    # b) Left Right Case
+    #
+    #      z                               z                           x
+    #     / \                            /   \                        /  \
+    #    y   T4  Left Rotate (y)        x    T4  Right Rotate(z)    y      z
+    #   / \      - - - - - - - - ->    /  \      - - - - - - - ->  / \    / \
+    # T1   x                          y    T3                    T1  T2 T3  T4
+    #     / \                        / \
+    #   T2   T3                    T1   T2
+    #
+    # c) Right Right Case
+    #
+    #   z                                y
+    #  /  \                            /   \
+    # T1   y     Left Rotate(z)       z      x
+    #     /  \   - - - - - - - ->    / \    / \
+    #    T2   x                     T1  T2 T3  T4
+    #        / \
+    #      T3  T4
+    #
+    # d) Right Left Case
+    #
+    #    z                            z                            x
+    #   / \                          / \                          /  \
+    # T1   y   Right Rotate (y)    T1   x      Left Rotate(z)   z      y
+    #     / \  - - - - - - - - ->     /  \   - - - - - - - ->  / \    / \
+    #    x   T4                      T2   y                  T1  T2  T3  T4
+    #   / \                              /  \
+    # T2   T3                           T3   T4
